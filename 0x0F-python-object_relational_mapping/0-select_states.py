7a17af7b64c96a6dc59cfdea2f14f042f499049b
calvean <garnetsharara@gmail.com>
2022-12-23 13:05:40 +0200

    first commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,5 +6,5 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     a = db.cursor()
-    a.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in a.fetchall() if state[1][0] == "N"]
+    a.execute("SELECT * FROM `states`")
+    [print(state) for state in a.fetchall()]
