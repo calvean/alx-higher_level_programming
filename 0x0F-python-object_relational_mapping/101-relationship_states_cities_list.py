b9b0cd54a5d576f23bc40335f9670a6d42f0c69f
calvean <garnetsharara@gmail.com>
2022-12-27 21:28:44 +0200

    fixes

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -20,8 +20,6 @@ if __name__ == '__main__':
 
     states = session.query(State).order_by(State.id).all()
     for state in states:
-        # <state id>: <state name>
-        # <tabulation><city id>: <city name>
         print('{}: {}'.format(state.id, state.name))
         for city in state.cities:
             print('\t{}: {}'.format(city.id, city.name))
