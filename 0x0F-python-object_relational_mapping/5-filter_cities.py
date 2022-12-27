cb3b8d978a31b4ebfe4ac5598044154924aa4447
calvean <garnetsharara@gmail.com>
2022-12-27 20:24:40 +0200

    fixes

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
 """
-takes in the name of a state as an argument and
-lists all cities of that state, using the database hbtn_0e_4_usa
+Displays all cities of a given state from the
+states table of the database hbtn_0e_4_usa.
 """
+import sys
 import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
-        charset="utf8",
-    )
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    stat = """SELECT cities.name
-                 FROM cities, states
-                 WHERE BINARY states.name = %s
-                 AND cities.state_id = states.id
-                 ORDER BY cities.id ASC"""
-    c.execute(stat, (argv[4],))
-    all_rows = cur.fetchall()
-    print(", ".join(row[0] for row in all_rows))
-    c.close()
-    db.close()
+    c.execute("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
