f439e0389213fe9ef7074bdc1de98e24f2f88ecc
calvean <garnetsharara@gmail.com>
2023-01-21 18:07:16 +0200

    task6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Takes in a URL and an email address, sends a POST request
+ to the passed URL with the email as a parameter, and
+ finally displays the body of the response.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    mail = {"email": sys.argv[2]}
+    req = requests.post(url, data=mail)
+    print(req.text)
