35c420660b93858f836f954ca451ce09a1dbe42b
calvean <garnetsharara@gmail.com>
2023-01-21 23:15:58 +0200

    fixes

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Takes GitHub credentials (username and password)
+and uses the GitHub API to display your id
+Basic Authentication with a personal access token as password
+ is used to access to your information
+ (only read:user permission is needed)
+"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    req = requests.get("https://api.github.com/user", auth=auth)
+
+    print(req.json().get("id"))
